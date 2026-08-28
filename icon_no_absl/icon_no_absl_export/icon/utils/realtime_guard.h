2f50a1f4e386d743a9c3005415df8182ee9d3e9c
tobit <tobit@google.com>
2026-08-28 09:58:57 +0000

    Replace ICON no_absl folder by external icon_shared_memory repository

--- icon_no_absl/icon_no_absl_export/icon/utils/realtime_guard.h
+++
@@ -1,7 +0,0 @@
-#ifndef ICON_UTILS_REALTIME_GUARD_H_
-#define ICON_UTILS_REALTIME_GUARD_H_
-
-// Annotation for a currently closed-source runtime realtime-safety guard.
-#define INTRINSIC_ASSERT_NON_REALTIME(x)
-
-#endif  // ICON_UTILS_REALTIME_GUARD_H_
