07624ec6d7f98e4c3b165da2a8f3cc960af061ef
tobit <tobit@google.com>
2026-08-31 10:12:00 +0000

    Fix dependencies for ICON shared memory vendor package

---
+++ icon_shared_memory_vendor/cmake/icon_shared_memory_vendor-extras.cmake
@@ -0,0 +1,7 @@
+# Required for transitive dependencies in downstream packages.
+# For more details see https://docs.ros.org/en/jazzy/How-To-Guides/Ament-CMake-Documentation.html#basic-project-outline
+find_package(Eigen3 REQUIRED)
+find_package(flatbuffers REQUIRED)
+find_package(icon_shared_memory REQUIRED)
+find_package(tl-expected REQUIRED)
+
